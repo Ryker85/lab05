8b87243d25f17283814aae917d8420b15ac05f09
Ryker85 <rykersaen52@gmail.com>
2026-09-16 16:42:43 +0000

    feat: add is_even function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+fn is_even(n: i32) -> bool {
+    if n % 2 == 0 {
+        return true;
+    } else {
+        return false;
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let number = 4;
+    println!("Is {} even? {}", number, is_even(number));
 }
